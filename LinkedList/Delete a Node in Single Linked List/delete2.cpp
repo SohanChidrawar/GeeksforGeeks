e28aa87b47b91abe9b706818e29cd26e44a5a354
Sohan Chidrawar <99244757+SohanChidrawar@users.noreply.github.com>
2024-03-04 19:22:00 +0530

    Create delete2.cpp

---
+++ LinkedList/Delete a Node in Single Linked List/delete2.cpp
@@ -0,0 +1,17 @@
+Node* deleteNode(Node *head, int x)
+{
+    //Your code here
+    if(x == 1){
+        return head->next ; 
+    }
+    int count = 1 ;
+    Node* curr = head ;
+    
+    while(count != x-1){
+        curr = curr->next ; 
+        count++ ;
+    }
+    curr->next = curr->next->next ; 
+    
+    return head ; 
+}
