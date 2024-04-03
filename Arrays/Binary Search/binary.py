025cb58585713670d35b248be3cdc8da6390c8bd
Sohan Chidrawar <99244757+SohanChidrawar@users.noreply.github.com>
2024-04-03 18:12:00 +0530

    Update binary.py

--- Arrays/Binary Search/binary.py
+++ Arrays/Binary Search/binary.py
@@ -5,13 +5,16 @@ def binarysearch(self, arr, n, k):
 		
 		while(low<=high):
 		    mid = (low+high)//2
-		    
+		    # If element is found at mid, return mid
 		    if(arr[mid]==k):
 		        return mid
-		        
+		       
+		    # If element is smaller than mid, search left half
 		    elif(arr[mid]>k):
 		        high=mid-1
-		       
+		    # If element is greater than mid, search right half
 		    else:
 		        low=mid+1
+		        
+		# If element is not found, return -1
 		return -1
