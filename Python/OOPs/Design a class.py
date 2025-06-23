a5562114bb882499c8b4e0d7a8d57694f96d5c1c
Sohan Chidrawar <99244757+SohanChidrawar@users.noreply.github.com>
2025-06-23 18:40:28 +0530

    Create Design a class.py

---
+++ Python/OOPs/Design a class.py
@@ -0,0 +1,26 @@
+# Design a class as described below.
+
+# class name : MyClass
+# function: function name - display(), parameters - none, return type - void , access modifier - public, function body - should print "Hello World"
+
+# Note: The driver's code will call the display function of the MyClass
+
+# Example:
+
+# Input: None
+# Output: "Hello World"
+
+#User function Template for python3
+# Design a class as described in the question
+# Code here
+
+# The call to display function is made like this
+# obj = MyClass()
+# obj.display()
+
+class MyClass:
+    def display(self):
+        print("Hello World")
+        
+# t = MyClass()
+# t.display()
