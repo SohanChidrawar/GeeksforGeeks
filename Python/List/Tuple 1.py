2a93cff5f6834687b4e6c435b862b8ed23947498
Sohan Chidrawar <99244757+SohanChidrawar@users.noreply.github.com>
2025-08-14 22:47:21 +0530

    Create Tuple 1.py

---
+++ Python/List/Tuple 1.py
@@ -0,0 +1,14 @@
+# You are given a tuple numbers that contains integers. You need to return a tuple containing doubles of given numbers.
+
+# Examples:
+
+# Input: tup = (4, 5, 1, 2, 3, 5)
+# Output: 8 10 2 4 6 10
+# Explanation: multiplied numbers by 2.
+# Input: tup = (3, 1, 4, 7, 2, 6, 4)
+# Output: 6 2 8 14 4 12 8
+# Explanation: multiplied number by 2.
+
+def doubleTup(numbers):
+    #code here
+    return tuple(n*2 for n in numbers)
