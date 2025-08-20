14f1749f0966f9768f7c3ceae151e3a5407f5cbe
Sohan Chidrawar <99244757+SohanChidrawar@users.noreply.github.com>
2025-08-20 18:02:50 +0530

    Create First Digit of a Number.py

---
+++ Functions/First Digit of a Number.py
@@ -0,0 +1,19 @@
+'''
+Given a number n, find the first digit of the number.
+
+Examples:
+
+Input: n = 123
+Output: 1
+Input: n = 976
+Output: 9
+
+Constraints:
+1 <= n <= 109
+'''
+
+def firstDigit(n):
+    #code here
+    while(n>=10):
+        n = n//10
+    return n
