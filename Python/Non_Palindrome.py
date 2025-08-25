55f0e90439edc9b66effd821edd5163c4b83aae3
Sohan Chidrawar <99244757+SohanChidrawar@users.noreply.github.com>
2025-08-25 22:17:50 +0530

    Create Non_Palindrome.py

---
+++ Python/Non_Palindrome.py
@@ -0,0 +1,9 @@
+# You are given a number n, you need to return the count of the total number of non palindromic number from 1 to n both inclusive.
+# Non palindromic number are those that don't read same from front and back
+
+def nonPalindrome(n):
+  count  = 0
+
+  if str(i) == str(i)[::-1]:
+    count+=1
+return count
